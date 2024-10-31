62d62a7d2cbd5356963f8a738b080f1a551b85d1
Taka-bnbn <mizutaka2005@gmail.com>
2024-10-31 12:15:26 +0900

    add a command

--- test.bash
+++ test.bash
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+ng (){
+	echo ${1}行目が違うぞボケ
+	res=1
+}
+
+res=0
 a=山田
-[ "$a" = 上田 ]
-echo $?
-[ "$a" = 山田 ]
-echo $?
+[ "$a" = 上田 ] || ng "$LINENO"
+[ "$a" = 山田 ] || ng "$LINENO"
+
+exit "$res"
