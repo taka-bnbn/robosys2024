de95622715e476fd183897543a5f43604d5d01c4
Taka-bnbn <mizutaka2005@gmail.com>
2024-10-31 12:42:56 +0900

    これはエラーが出たら正解？

--- test.bash
+++ test.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+# SPDX-FileCopyrightText :2024 Takaya Mizumaki
+# SPDX-License-Identifier: BSD-3=Clause
+
 
 ng (){
 	echo ${1}行目が違うぞボケ
@@ -7,9 +10,18 @@ ng (){
 
 res=0
 
+###NORMAL INPUT###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+###STRANGE INPUT###
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]	 || ng "$LINENO"
+[ "${out}" = "" ]|| ng "$LINENO"
 
+out=$(echo | ./plus)
+[ "$?" = 1 ] 	|| ng "$LINENO"
+[ "${out}" =  ] || ng "$LINENO"
+
+["$res" = 0 ]  && echo OK
 exit "$res"
